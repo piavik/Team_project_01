c6f6844ece0f093074d215b4ed4814ce4cc436da
piavik <piavik@gmail.com>
2023-11-12 01:07:58 +0100

    add birthday separated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from classes import Record, AddressBook
 from types import GeneratorType
+from datetime import datetime
+from classes import Record, AddressBook
 import folder_sort
 
 
@@ -32,7 +33,7 @@ def input_error(func):
         try:
             result = func(*args)
         except KeyError:
-            result = "Not found. Unknown record."
+            result = "Not found."
         except ValueError:
             result = "Entered incorrect data"
         except IndexError:
@@ -48,32 +49,75 @@ def hello(*args):
     return BLUE + "How can I help you?" + RESET
 
 @input_error
-def add_(contact_name, new_phone, birthday=None):
-    # with "birthday" field it is necessary to remove the feature 
-    # to add several phones at a time
-    # so phones are added one by one now
-    # if exist - we add phone to the list, not replace
+def add_phone(contact_name: str, *args, **kwargs) -> str:
+    if len(args) > 0:
+        new_phone = args[0]
+    else:
+        new_phone = input(f'{GREEN}Please enter the phone number (10 digits): {RESET}')
+    # if contact exist - we add phone to the list, not replace
     if contact_name in address_book.data.keys():
         record = address_book.data[contact_name]
-        if birthday:
-            record.add_birthday(birthday)
         record.add_phone(new_phone)
     else:
         record = Record(contact_name)
-        if birthday:
-            record.add_birthday(birthday)
         record.add_phone(new_phone)
         address_book.add_record(record)
+    #this should be corrected when there are other fields added
+    if len(args) >=2 :
+        birthday = args[1]
+        record.add_birthday(birthday)
     message = f"\n{GREEN}Record added:\n  {RESET}Name: {record.name.value}\n  phone: {new_phone}"
     return message
 
 @input_error
+def add_birthday(contact_name: str, *args, **kwargs) -> str:
+    if len(args) > 0:
+        birthday = args[0]
+    else:
+        birthday = input(f'{GREEN}Please enter birthday (YYYY-MM-DD): {RESET}')
+    if contact_name in address_book.data.keys():
+        record = address_book.data[contact_name]
+        record.add_birthday(birthday)
+    else:
+        # entered date instead of contact name
+        if datetime.strptime(contact_name, '%Y-%m-%d'):
+            raise ValueError
+        record = Record(contact_name)
+        record.add_birthday(birthday)
+        address_book.add_record(record)
+    message = f"\n{GREEN}Record added:\n  {RESET}Name: {record.name.value}\n  birthday: {birthday}"
+    return message
+
+@input_error
 def change_phone(*args):
-    contact_name = args[0]
-    old_phone, new_phone = args[1:]
+    if len(args) == 1:
+        contact_name = args[0]
+    else: 
+        contact_name = input(f'{GREEN}Please enter contact name: {RESET}')
     record = address_book.data[contact_name]
+    if len(args) == 3:
+        old_phone, new_phone = args[1:2]
+    else:
+        old_phone = input(f'{GREEN}Please enter old number: {RESET}')
+        if not old_phone in record.phones:
+            raise KeyError
+        new_phone = input(f'{GREEN}Please enter new number (10 digits): {RESET}')
     record.edit_phone(old_phone, new_phone)
-    return f"\n{GREEN}Changed:\n  {RESET}{record.name.value}\n  {old_phone} to {new_phone}"
+    return f"\n{GREEN}Changed:\n  {RESET}Name: {record.name.value}\n  Phone: {old_phone} to {new_phone}"
+
+@input_error
+def change_birthday(*args):
+    if len(args) > 0:
+        contact_name = args[0]
+    else:
+        contact_name = input(f'{GREEN}Please enter contact name: {RESET}')
+    record = address_book.data[contact_name]
+    if len(args) == 3:
+        old_birthday, new_birthday = args[1:]
+    else:
+        new_birthday = input(f'{GREEN}Please enter new birthday date (YYYY-MM-DD): {RESET}')
+    record.add_birthday(new_birthday)
+    return f"\n{GREEN}Changed to:{RESET} {new_birthday}"
 
 @input_error
 def get_phone(*args):
@@ -173,16 +217,18 @@ OPERATIONS = {
                 "hi": hello,
                 "help": help_,
                 "?": hello,
-                "add entry": add_,
-                "add record": add_,
-                "add number": add_,
-                "add phone": add_,
-                "add": add_,
-                "set": add_,
+                "add entry": add_phone,
+                "add record": add_phone,
+                "add number": add_phone,
+                "add phone": add_phone,
+                "add birthday": add_birthday,
+                "add": add_phone,
+                "set": add_phone,
                 "change entry": change_phone,
                 "change record": change_phone,
                 "change number": change_phone,
                 "change phone": change_phone,
+                "change birthday": change_birthday,
                 "change": change_phone, 
                 "get entry": get_phone,
                 "get record": get_phone,
@@ -198,8 +244,8 @@ OPERATIONS = {
                 "list all": all_contacts,
                 "full": all_contacts,
                 "list": all_contacts,
-                "del": delete_phone,
                 "delete": delete_phone,
+                "del": delete_phone,
                 "remove": delete_phone,
                 # "d": debug_,
                 "read": restore_data_from_file,
