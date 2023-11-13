d389b92c4dbe61538de79c985d4729bd1611cffc
Tetiana Kobzar <tkobzar.smm@gmail.com>
2023-11-13 09:50:26 +0000

    read from file

--- main.py
+++ main.py
@@ -84,48 +84,10 @@ def all_contacts(N=3, *args):
     return address_book.iterator(N)
 
 def help_(*args):
-    return BLUE + """
-    This is the CLI Address Book - an application that will simplify your work with contact information and save your time for more interesting cases.
-    Updated and improved by "Bot farm " team.
-    When using command, note the following features:
-     * (required information to execute the command).
-     * the input information should be separated by space.
-     * Use date format " YYYY-MM-DD".
-    Commands to work with address book:
-        "hello" or "hi"     - greeting
-        "help" or "?"       - calling for help
-        "show all" or "all" - display all the contents of the phone book.
-        "add phone" or "add"- (name and phone)  - add the phone to the contact. 
-                              If the contact is missing create a new 
-        "change phone" - (name, old phone, new phone) - edit phone record.
-        "get contakt"  - (name) - show all contact information.
-        "delete"       - (name) - delete a contact.
-        "delete"       - (phone) - delete the phone.
-        "load"         - loading of adpes book from file.
-        "save"         - saving the address book to a file.
-        "find"         - (string) search for matches in the address book, 
-                         taking into account all contact fields
-        "birthdays"    - (number of days) - show the contacts who have a birthday
-                         in the coming days.
-        "add email"    - (name and email) - add the email to the contact. 
-                         If the contact does not exist, a new contact is created.
-        "change email" - (name, old email and new email) - change email.
-        "delete email" - (name) -  remove email from contact.
-        "add address"   - (name and address) - add address to contact.
-        "change address"- (name and new address) - change address.
-        "delete address"- (name and address) - delete adress from contact.
-        "exit"          - the end of the work.
-    Commands to work with notes:     
-        "add note"     - add notes.
-        "add tags"     - add tags.
-        "change note"  - change notes.
-        "delete note" or "del note" - delete notes. 
-        "delete tags" or "del tags" - remove tags.        
-        "find note"    - find note.
-        "sort notes"   - sort notes by tag.
-    Command to organize files in a folder:        
-        "sort folder" or "sort" - (name folder) - organize files in the specified folder
-    """ + RESET
+    with open('README.txt', 'r') as fh:
+        help_bot = fh.read()
+    return help_bot
+    
 
 def unknown_command(*args):
     return f"{RED}I do not understand, please use correct command.{RESET}"
