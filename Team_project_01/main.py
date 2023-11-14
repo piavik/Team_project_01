3cb595cf407d856c3802a959e1ad02ce2300161c
RomanSiu <siusiailoroman@gmail.com>
2023-11-14 22:04:23 +0200

    fix Word_Completer

--- Team_project_01/main.py
+++ Team_project_01/main.py
@@ -475,7 +475,7 @@ OPERATIONS = {
               }
 
 ALL_COMMANDS = OPERATIONS.keys()
-command_completer = WordCompleter(ALL_COMMANDS)
+command_completer = WordCompleter(ALL_COMMANDS, sentence=True)
 
 def parse(input_text: str):
     # itereate over keywords dict, not over input words !!!
