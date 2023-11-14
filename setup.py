51180715ea16fdb9a66b6bb3d3d9535d1720a9d5
piavik <piavik@gmail.com>
2023-11-14 21:56:37 +0100

    setup fix

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='Team_project_01',
-    version='0.4.2',
+    version='0.4.4',
     description='ClI Assistant',
     url='https://github.com/piavik/Team_project_01',
     author='project-group4',
     license='MIT',
     packages=find_namespace_packages(),
     install_requires=['prompt_toolkit'],
+    include_package_data=True,
     entry_points={'console_scripts': ['Informator = Team_project_01.main:main']}
 )
